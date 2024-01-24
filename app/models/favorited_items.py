531c0512af4da7da165217059a85d7acd853b551
Brandon <b.n.tamayo7@gmail.com>
2024-01-24 14:19:11 -0800

    Adjusted env.py and __init__ files for render

--- app/models/favorited_items.py
+++ app/models/favorited_items.py
@@ -3,6 +3,9 @@ from .db import db
 class FavoritedItem(db.Model):
     __tablename__ = "favoriteditems"
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey("users.id"))
     productId = db.Column(db.Integer, db.ForeignKey("products.id"))
--- migrations/versions/d54c900bbd5c_.py
+++ migrations/versions/d54c900bbd5c_.py
@@ -1,12 +1,15 @@
 """empty message
 
 Revision ID: d54c900bbd5c
-Revises: 
+Revises:
 Create Date: 2024-01-23 16:38:14.335268
 
 """
 from alembic import op
 import sqlalchemy as sa
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -92,6 +95,20 @@ def upgrade():
     sa.ForeignKeyConstraint(['userId'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE products SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE orders SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE favoriteditems SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE order_items SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE product_images SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE reviews SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
