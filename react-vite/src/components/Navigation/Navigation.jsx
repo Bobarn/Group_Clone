44bd730f8e83d11af9587bd7668f479e71df8aff
Kyrene <Kyrene.Aflores@gmail.com>
2024-01-25 14:33:51 -0600

    finished functionality of navigation bar

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,24 +1,4 @@
-// import { NavLink } from "react-router-dom";
-// import ProfileButton from "./ProfileButton";
-// import "./Navigation.css";
-
-// function Navigation() {
-//   return (
-//     <ul>
-//       <li>
-
-//         <NavLink to="/">Home</NavLink>
-//       </li>
-
-//       <li>
-//         <ProfileButton />
-//       </li>
-//     </ul>
-//   );
-// }
-
-// export default Navigation;
-import { NavLink } from "react-router-dom";
+import {useState} from "react"
 import { useNavigate } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import CategoriesMenu from "./CategoriesMenu";
@@ -27,19 +7,25 @@ import "./Navigation.css";
 function Navigation() {
   // const logoImg = 'https://cdn.discordapp.com/attachments/1196941900939280534/1199775281427259453/logo-color.png?ex=65c3c4cd&is=65b14fcd&hm=10ffed8b450e1dea968323412876fe440315bd9f87d5d771df14efd2cde949e2&'
   const navigate = useNavigate();
-  const logoImg =
-    "https://cdn.discordapp.com/attachments/1196941900939280534/1199775281888637070/logo-no-background.png?ex=65c3c4cd&is=65b14fcd&hm=f65bd9aa5eadabc00111d0f96b564b8d2d7d34addfcd9625464f9a85ac476dd2&";
-
+  const logoImg ="https://cdn.discordapp.com/attachments/1196941900939280534/1199775281888637070/logo-no-background.png?ex=65c3c4cd&is=65b14fcd&hm=f65bd9aa5eadabc00111d0f96b564b8d2d7d34addfcd9625464f9a85ac476dd2&";
+  const [value,setValue] = useState()
   return (
     <div className="nav-main-cont" >
       <div className="logo-main-cont" onClick={() => navigate("/")}>
         <img src={logoImg} alt="Saved Image" className="logo-img" />
       </div>
+      <div className='nav-cat-comp-cont'>
       <CategoriesMenu />
-      {/* <div className="nav-cat-main-cont">
-        <i className="fa-solid fa-bars"></i>
-        <span>Categories</span>
-      </div> */}
+      </div>
+      <div className='nav-search-main-cont'>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Search..."
+      />
+
+      </div>
 
       <div>
         <ProfileButton />
