f303e8d8765cc19d962a7f8599d456c653a0e2f8
Kyrene <Kyrene.Aflores@gmail.com>
2024-01-25 19:48:37 -0600

    initial commit

--- app/models/favorited_items.py
+++ app/models/favorited_items.py
@@ -6,6 +6,9 @@ class FavoritedItem(db.Model):
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
     productId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("products.id")))
