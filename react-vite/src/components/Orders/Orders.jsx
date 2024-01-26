d3904e1afd546a91a30f3c39c908ca2d9aea0693
Kyrene <Kyrene.Aflores@gmail.com>
2024-01-26 12:08:55 -0600

    finished orders page

--- react-vite/src/components/Orders/Orders.jsx
+++ react-vite/src/components/Orders/Orders.jsx
@@ -1,54 +1,55 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { thunkGetAllOrders } from "../../redux/orders";
+import {useNavigate} from "react-router-dom"
 import "./orders.css";
 
 function Orders() {
   const dispatch = useDispatch();
+  const navigate = useNavigate()
   const allOrders = useSelector((state) => state.orders);
   const orders = Object.values(allOrders);
 
-  console.log("ORDERS", orders);
+
   useEffect(() => {
     dispatch(thunkGetAllOrders());
   }, [dispatch]);
 
   return (
     <div className="order-page-main-cont">
+      <div className="order-title-cont">
         <span>Your purchases</span>
+      </div>
 
       <div className="orders-main-cont">
-        <div className="order-cont">
-          {orders.map((order) => (
-            <div key={order.id}>
-              {order.items.map((item) => (
-                <div className="order-item-cont" key={item.id}>
-                  <div className="order-top-cont">
-                    <span>{`Purchased from ${
-                      item.product.seller.first_name
-                    } on ${order.purchase_date
-                      .split(" ")
-                      .slice(0, 4)
-                      .join(" ")}`}</span>
-                    <span>{`$${item.product.price}`}</span>
-                  </div>
+        {orders.map((order) => (
+          <div key={order.id} className="full-order-cont">
+            <div className='date-cont'>
+              <span>{`Purchased on ${order.purchase_date
+                .split(" ")
+                .slice(0, 4)
+                .join(" ")}`}</span>
+                <div className='order-total-cont'>
+                    <span>{`$${order.total}`}</span>
 
-                  <div className="order-lower-cont">
-                    <img
-                      className="order-img"
-                      src={item.product.preview_image}
-                    />
-                    <div className='order-name-cont'>
+                </div>
+            </div>
+            {order.items.map((item) => (
+              <div className="order-item-cont" key={item.id}>
+                <div className="order-top-cont">
+                  {/* <span>{`$${item.product.price}`}</span> */}
+                </div>
+
+                <div className="order-lower-cont">
+                  <img className="order-img" src={item.product.preview_image}  />
+                  <div className="order-name-cont">
                     <span>{item.product.name}</span>
-                    {/* <button>Add your review</button> */}
-
-                    </div>
                   </div>
                 </div>
-              ))}
-            </div>
-          ))}
-        </div>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
